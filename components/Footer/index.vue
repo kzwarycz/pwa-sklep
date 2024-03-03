e815b3f47d5ace136a9f2c41c1dfd76fa3ef7abe
Kamil 74684 <kzwarycz@antmicro.com>
2024-03-03 14:49:23 +0100

    Add improved Footer

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,16 +1,53 @@
 <template>
-  <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        <strong>Bulma</strong> by
-        <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
-        licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
-        website content is licensed
-        <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
-          >CC BY NC SA 4.0</a
-        >.
-      </p>
-    </div>
-  </footer>
-</template>
+    <footer class="footer">
+      <div class="content">
+        <p class="description">
+          <strong>DALL·E 2 Clothing</strong> - odkryj unikalną kolekcję odzieży inspirowaną sztuczną inteligencją. Nasze projekty łączą innowacyjność z modą, tworząc niepowtarzalny styl dla każdego.
+        </p>
+        <div class="social">
+          <span>Śledź nas na:</span>
+          <a href="https://www.linkedin.com/company/dall-e-2-clothing">
+            <img src="./linkedIn.svg" alt="LinkedIn" width="32" height="32">
+          </a>
+          <a href="https://www.instagram.com/dall-e-2-clothing">
+            <img src="./instagram.svg" alt="Instagram" width="32" height="32">
+          </a>
+        </div>
+      </div>
+    </footer>
+  </template>
+  
+  <style scoped>
+  .footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #fff;
+    color: black;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .description {
+    margin: 0;
+    text-align: center;
+  }
+  
+  .social {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end; /* Wyrównuje elementy .social do prawej strony */
+}
+  
+  .social span {
+    margin-right: 10px;
+  }
+  
+  .social a {
+    margin-left: 10px;
+  }
+  </style>
+  
