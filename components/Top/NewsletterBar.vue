5a56ba7b194213d749ec3ec61db97d682de8e73f
Kamil 74684 <kzwarycz@antmicro.com>
2024-03-09 19:10:51 +0100

    Add NewsletterBar component

---
+++ components/Top/NewsletterBar.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+<template>
+  <div class="info">
+    <slot name="content">Newsletter info</slot>
+  </div>
+  <TopNewsletterDrawer />
+</template>
+<style scoped lang="scss">
+.info {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  background-color: #000;
+  color: #fff;
+  span {
+    margin-right: 10px;
+  }
+}
+</style>
