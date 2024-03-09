266b8aef2a401710cb17cb7aceb47ca849b802c9
Kamil 74684 <kzwarycz@antmicro.com>
2024-03-09 20:14:03 +0100

    Replace bulma with shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,16 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/main.scss'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
 })
