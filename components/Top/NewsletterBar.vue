67fbfbb4daf527200449558775a50b9debbe7fa4
Kamil 74684 <kzwarycz@antmicro.com>
2024-03-09 20:15:39 +0100

    Add Close button to NewsltetterBar

--- components/Top/NewsletterBar.vue
+++ components/Top/NewsletterBar.vue
@@ -2,9 +2,36 @@
 <template>
   <div class="info">
     <slot name="content">Newsletter info</slot>
+    <TopNewsletterDrawer />
+    <button class="close-btn" @click="closeComponent">
+      <svg
+        width="15"
+        height="15"
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z"
+          fill="currentColor"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
   </div>
-  <TopNewsletterDrawer />
 </template>
+
+<script setup>
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["close"]);
+
+function closeComponent() {
+  emit("close");
+}
+</script>
+
 <style scoped lang="scss">
 .info {
   display: flex;
@@ -13,11 +40,17 @@
   align-items: center;
   align-items: center;
   width: 100%;
+  border: 1px solid #e0e0e0;
   height: 50px;
-  background-color: #000;
-  color: #fff;
-  span {
-    margin-right: 10px;
-  }
+}
+
+.close-btn {
+  position: absolute;
+  right: 10px;
+  top: 18px;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+  font-size: 20px;
 }
 </style>
