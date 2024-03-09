7ec691caa025c27b3107baf4914eba6b3f73fd2b
Kamil 74684 <kzwarycz@antmicro.com>
2024-03-09 19:09:22 +0100

    Fix footer width

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,53 +1,60 @@
 <template>
-    <footer class="footer">
-      <div class="content">
-        <p class="description">
-          <strong>DALL·E 2 Clothing</strong> - odkryj unikalną kolekcję odzieży inspirowaną sztuczną inteligencją. Nasze projekty łączą innowacyjność z modą, tworząc niepowtarzalny styl dla każdego.
-        </p>
-        <div class="social">
-          <span>Śledź nas na:</span>
-          <a href="https://www.linkedin.com/company/dall-e-2-clothing">
-            <img src="./linkedIn.svg" alt="LinkedIn" width="32" height="32">
-          </a>
-          <a href="https://www.instagram.com/dall-e-2-clothing">
-            <img src="./instagram.svg" alt="Instagram" width="32" height="32">
-          </a>
-        </div>
+  <footer class="footer">
+    <div class="content">
+      <p class="description">
+        <strong>DALL·E 2 Clothing</strong> - odkryj unikalną kolekcję odzieży
+        inspirowaną sztuczną inteligencją. Nasze projekty łączą innowacyjność z
+        modą, tworząc niepowtarzalny styl dla każdego.
+      </p>
+      <div class="social">
+        <span>Śledź nas na:</span>
+        <a href="https://www.linkedin.com/company/dall-e-2-clothing">
+          <img src="./linkedIn.svg" alt="LinkedIn" width="32" height="32" />
+        </a>
+        <a href="https://www.instagram.com/dall-e-2-clothing">
+          <img src="./instagram.svg" alt="Instagram" width="32" height="32" />
+        </a>
       </div>
-    </footer>
-  </template>
-  
-  <style scoped>
-  .footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    background-color: #fff;
-    color: black;
-    padding: 10px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  
-  .description {
-    margin: 0;
-    text-align: center;
-  }
-  
-  .social {
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  color: black;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.content {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.description {
+  margin: 0;
+  text-align: center;
+}
+
+.social {
   display: flex;
   align-items: center;
-  justify-content: flex-end; /* Wyrównuje elementy .social do prawej strony */
+  justify-content: flex-end;
 }
-  
-  .social span {
-    margin-right: 10px;
-  }
-  
-  .social a {
-    margin-left: 10px;
-  }
-  </style>
-  
+
+.social span {
+  margin-right: 10px;
+}
+
+.social a {
+  margin-left: 10px;
+}
+</style>
